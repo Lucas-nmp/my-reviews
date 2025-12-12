81fb976da25f08992336aba23532f1549109d203
Lucas-nmp <lucas.morandeira.parejo@gmail.com>
2025-12-12 12:27:53 +0100

    build: main page design and refactor menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
-import { IonApp, IonRouterOutlet, IonMenu, IonHeader, IonToolbar, IonTitle, IonContent, IonList, IonMenuToggle, IonItem, IonLabel, IonIcon } from '@ionic/angular/standalone';
+import { IonApp, IonRouterOutlet, IonMenu, IonHeader, IonToolbar, IonTitle, IonContent, IonList, IonMenuToggle, IonItem, IonLabel } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  imports: [IonApp, IonRouterOutlet, RouterLink, IonMenu, IonHeader, IonToolbar, IonTitle, IonContent, IonList, IonMenuToggle, IonItem, IonLabel, IonIcon],
+  imports: [IonApp, IonRouterOutlet, RouterLink, IonMenu, IonHeader, IonToolbar, IonTitle, IonContent, IonList, IonMenuToggle, IonItem, IonLabel],
 })
 export class AppComponent {
   constructor() {}
--- src/app/pages/main/main.page.ts
+++ src/app/pages/main/main.page.ts
@@ -1,18 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonButtons, IonMenuButton } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonButtons, IonMenuButton, IonRow, IonCol, IonIcon } from '@ionic/angular/standalone';
+import { RouterLink } from '@angular/router';
+import { bookOutline, personOutline, filmOutline, logOutOutline, settingsOutline } from 'ionicons/icons';
+import { addIcons } from 'ionicons';
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.page.html',
   styleUrls: ['./main.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonButtons, IonMenuButton]
+  imports: [IonContent, IonHeader, RouterLink, IonTitle, IonToolbar, CommonModule, FormsModule, IonButtons, IonMenuButton, IonRow, IonCol, IonIcon]
 })
 export class MainPage implements OnInit {
 
-  constructor() { }
+  constructor() {
+    addIcons ({
+      bookOutline, personOutline, filmOutline, logOutOutline, settingsOutline
+    })
+   }
 
   ngOnInit() {
   }
