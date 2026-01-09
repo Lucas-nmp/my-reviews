666ae8ad3a5fb84bd8ad36a4127421a6ac95bbab
lucas-nmp <lucas.morandeira.parejo@gmail.com>
2026-01-09 22:03:22 +0100

    build: add structure and style to the new movie component

--- src/app/components/movie-new-modal/movie-new-modal.component.ts
+++ src/app/components/movie-new-modal/movie-new-modal.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
-import { ModalController, IonHeader, IonToolbar, IonTitle, IonButtons, IonButton, IonContent, IonIcon } from '@ionic/angular/standalone';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormControl, FormGroup, Validators, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { IonContent, IonHeader, IonToolbar, IonTitle, IonButtons, IonButton, ModalController, IonIcon, IonLabel, IonDatetimeButton, IonModal, IonDatetime, IonInput, IonRow, IonCol, IonTextarea } from "@ionic/angular/standalone";
 import { addIcons } from 'ionicons';
 import { closeOutline } from 'ionicons/icons';
+
 
 @Component({
   selector: 'app-movie-new-modal',
   templateUrl: './movie-new-modal.component.html',
   styleUrls: ['./movie-new-modal.component.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonButtons, IonButton, IonContent, IonIcon],
+  standalone: true,
+  imports: [CommonModule, IonContent, IonHeader, IonToolbar, IonTitle, IonButtons, IonButton, IonIcon, ReactiveFormsModule, FormsModule, IonLabel, IonDatetimeButton, IonModal, IonDatetime, IonInput, IonRow, IonCol, IonTextarea],
 })
 export class MovieNewModalComponent  implements OnInit {
+ 
+    form = new FormGroup({
+    title: new FormControl('', [ Validators.required]),
+    protagonist: new FormControl('', [Validators.required]),
+    viewDate: new FormControl(new Date().toISOString(), [Validators.required]),
+    publicationYear: new FormControl(new Date().toISOString(), [Validators.required]),
+    review: new FormControl('', [Validators.required]),
+    image: new FormControl('', [Validators.required]),
+  })
 
   constructor(private modalCtrl: ModalController) {
     addIcons({
@@ -18,6 +31,21 @@ export class MovieNewModalComponent  implements OnInit {
    }
 
   ngOnInit() {}
+
+  @ViewChild('viewDateModal', { static: false }) viewDateModal?: IonModal;
+  @ViewChild('publicationYearModal', { static: false }) publicationYearModal?: IonModal;
+
+  onViewDateSelected() {
+    if (this.viewDateModal && typeof this.viewDateModal.dismiss === 'function') {
+      this.viewDateModal.dismiss();
+    }
+  }
+
+  onPublicationYearSelected() {
+    if (this.publicationYearModal && typeof this.publicationYearModal.dismiss === 'function') {
+      this.publicationYearModal.dismiss();
+    }
+  }
 
   close() {
     this.modalCtrl.dismiss();
